923b5a3ed4b758e7c2d3544a8b4f02aa2f5e2742
Bambi Brewer <bambi@birdbraintechnologies.com>
2020-05-27 08:07:14 -0400

    Initial Commit

---
+++ FinchBasicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinchBasicApp
+//
+//  Created by BirdBrain on 5/27/20.
+//  Copyright © 2020 BirdBrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
